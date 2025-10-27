9a810824edb81cf1956cdbde88466d3bd8b7d0b0
lyucean <lyucean@gmail.com>
2025-10-27 16:23:21 +0300

    Изменение основной цветовой схемы админ панели: замена цвета 'primary' с Amber на Blue в конфигурации AdminPanelProvider.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->favicon(asset('img/favicon.ico'))
             ->maxContentWidth('8xl')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
