c9fbd5427179eb0f224c32aa64c5fbd2f4631ab5
lyucean <lyucean@gmail.com>
2025-10-27 15:38:01 +0300

    Исправление namespace для действий в Filament 4.1: Tables\Actions -> Actions

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -107,12 +107,12 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
-                \Filament\Tables\Actions\EditAction::make()
+                \Filament\Actions\EditAction::make()
                     ->label('Редактировать'),
             ])
             ->bulkActions([
-                \Filament\Tables\Actions\BulkActionGroup::make([
-                    \Filament\Tables\Actions\DeleteBulkAction::make()
+                \Filament\Actions\BulkActionGroup::make([
+                    \Filament\Actions\DeleteBulkAction::make()
                         ->label('Удалить выбранных'),
                 ]),
             ])
